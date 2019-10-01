66bf3f7f6e7ceb9d01b27ed33243e2759c4448d4
Bruno Souza de Lima <bruno.lima@sinmec.ufsc.br>
2019-10-01 09:20:02 -0300

    AIMA

--- AIMA/Problem.py
+++ AIMA/Problem.py
@@ -1,18 +1,61 @@
-class Problem:
+"""Search (Chapters 3-4)
+The way to use this code is to subclass Problem to create a class of problems,
+then create problem instances and solve them with calls to the various search
+functions."""
+
+import bisect
+import math
+import random
+import sys
+from collections import deque
+
+from utils import (
+    is_in, argmin, argmax, argmax_random_tie, probability, weighted_sampler,
+    memoize, print_table, open_data, PriorityQueue, name,
+    distance, vector_add
+)
+
+infinity = float('inf')
+
+
+# ______________________________________________________________________________
+
+
+class Problem(object):
+    """The abstract class for a formal problem. You should subclass
+    this and implement the methods actions and result, and possibly
+    __init__, goal_test, and path_cost. Then you will create instances
+    of your subclass and solve them with the various search functions."""
 
     def __init__(self, initial, goal=None):
+        """The constructor specifies the initial state, and possibly a goal
+        state, if there is a unique goal. Your subclass's constructor can add
+        other arguments."""
         self.initial = initial
         self.goal = goal
 
-    def successor(self, state):
-        """Given a state, return a sequence of (action, state) pairs reachable
-        from this state. If there are many successors, consider an iterator
-        that yields the successors one at a time, rather than building them
-        all at once. Iterators will work fine within the framework."""
-        #abstract
+    def actions(self, state):
+        """Return the actions that can be executed in the given
+        state. The result would typically be a list, but if there are
+        many actions, consider yielding them one at a time in an
+        iterator, rather than building them all at once."""
+        raise NotImplementedError
+
+    def result(self, state, action):
+        """Return the state that results from executing the given
+        action in the given state. The action must be one of
+        self.actions(state)."""
+        raise NotImplementedError
 
     def goal_test(self, state):
-        return state == self.goal
+        """Return True if the state is a goal. The default method compares the
+        state to self.goal or checks for state in self.goal if it is a
+        list, as specified in the constructor. Override this method if
+        checking against a single self.goal is not enough."""
+        if isinstance(self.goal, list):
+            return is_in(state, self.goal)
+        else:
+            return state == self.goal
 
     def path_cost(self, c, state1, action, state2):
         """Return the cost of a solution path that arrives at state2 from
@@ -22,7 +65,442 @@ class Problem:
         and action. The default method costs 1 for every step in the path."""
         return c + 1
 
-    def value(self):
-        """For optimization problems, each state has a value.  Hill-climbing
+    def value(self, state):
+        """For optimization problems, each state has a value. Hill-climbing
         and related algorithms try to maximize this value."""
-        #abstract
+        raise NotImplementedError
+
+
+# ______________________________________________________________________________
+
+
+class Node:
+    """A node in a search tree. Contains a pointer to the parent (the node
+    that this is a successor of) and to the actual state for this node. Note
+    that if a state is arrived at by two paths, then there are two nodes with
+    the same state.  Also includes the action that got us to this state, and
+    the total path_cost (also known as g) to reach the node.  Other functions
+    may add an f and h value; see best_first_graph_search and astar_search for
+    an explanation of how the f and h values are handled. You will not need to
+    subclass this class."""
+
+    def __init__(self, state, parent=None, action=None, path_cost=0):
+        """Create a search tree Node, derived from a parent by an action."""
+        self.state = state
+        self.parent = parent
+        self.action = action
+        self.path_cost = path_cost
+        self.depth = 0
+        if parent:
+            self.depth = parent.depth + 1
+
+    def __repr__(self):
+        return "<Node {}>".format(self.state)
+
+    def __lt__(self, node):
+        return self.state < node.state
+
+    def expand(self, problem):
+        """List the nodes reachable in one step from this node."""
+        return [self.child_node(problem, action)
+                for action in problem.actions(self.state)]
+
+    def child_node(self, problem, action):
+        """[Figure 3.10]"""
+        next_state = problem.result(self.state, action)
+        next_node = Node(next_state, self, action,
+                         problem.path_cost(self.path_cost, self.state,
+                                           action, next_state))
+        return next_node
+
+    def solution(self):
+        """Return the sequence of actions to go from the root to this node."""
+        return [node.action for node in self.path()[1:]]
+
+    def path(self):
+        """Return a list of nodes forming the path from the root to this node."""
+        node, path_back = self, []
+        while node:
+            path_back.append(node)
+            node = node.parent
+        return list(reversed(path_back))
+
+    # We want for a queue of nodes in breadth_first_graph_search or
+    # astar_search to have no duplicated states, so we treat nodes
+    # with the same state as equal. [Problem: this may not be what you
+    # want in other contexts.]
+
+    def __eq__(self, other):
+        return isinstance(other, Node) and self.state == other.state
+
+    def __hash__(self):
+        return hash(self.state)
+
+
+# ______________________________________________________________________________
+
+
+class SimpleProblemSolvingAgentProgram:
+    """Abstract framework for a problem-solving agent. [Figure 3.1]"""
+
+    def __init__(self, initial_state=None):
+        """State is an abstract representation of the state
+        of the world, and seq is the list of actions required
+        to get to a particular state from the initial state(root)."""
+        self.state = initial_state
+        self.seq = []
+
+    def __call__(self, percept):
+        """[Figure 3.1] Formulate a goal and problem, then
+        search for a sequence of actions to solve it."""
+        self.state = self.update_state(self.state, percept)
+        if not self.seq:
+            goal = self.formulate_goal(self.state)
+            problem = self.formulate_problem(self.state, goal)
+            self.seq = self.search(problem)
+            if not self.seq:
+                return None
+        return self.seq.pop(0)
+
+    def update_state(self, state, percept):
+        raise NotImplementedError
+
+    def formulate_goal(self, state):
+        raise NotImplementedError
+
+    def formulate_problem(self, state, goal):
+        raise NotImplementedError
+
+    def search(self, problem):
+        raise NotImplementedError
+
+
+# ______________________________________________________________________________
+# Uninformed Search algorithms
+
+
+def breadth_first_tree_search(problem):
+    """Search the shallowest nodes in the search tree first.
+        Search through the successors of a problem to find a goal.
+        The argument frontier should be an empty queue.
+        Repeats infinitely in case of loops. [Figure 3.7]"""
+
+    frontier = deque([Node(problem.initial)])  # FIFO queue
+
+    while frontier:
+        node = frontier.popleft()
+        if problem.goal_test(node.state):
+            return node
+        frontier.extend(node.expand(problem))
+    return None
+
+
+def depth_first_tree_search(problem):
+    """Search the deepest nodes in the search tree first.
+        Search through the successors of a problem to find a goal.
+        The argument frontier should be an empty queue.
+        Repeats infinitely in case of loops. [Figure 3.7]"""
+
+    frontier = [Node(problem.initial)]  # Stack
+
+    while frontier:
+        node = frontier.pop()
+        if problem.goal_test(node.state):
+            return node
+        frontier.extend(node.expand(problem))
+    return None
+
+
+def depth_first_graph_search(problem):
+    """Search the deepest nodes in the search tree first.
+        Search through the successors of a problem to find a goal.
+        The argument frontier should be an empty queue.
+        Does not get trapped by loops.
+        If two paths reach a state, only use the first one. [Figure 3.7]"""
+    frontier = [(Node(problem.initial))]  # Stack
+    explored = set()
+    while frontier:
+        node = frontier.pop()
+        if problem.goal_test(node.state):
+            return node
+        explored.add(node.state)
+        frontier.extend(child for child in node.expand(problem)
+                        if child.state not in explored and
+                        child not in frontier)
+    return None
+
+
+def breadth_first_graph_search(problem):
+    """[Figure 3.11]
+    Note that this function can be implemented in a
+    single line as below:
+    return graph_search(problem, FIFOQueue())
+    """
+    node = Node(problem.initial)
+    if problem.goal_test(node.state):
+        return node
+    frontier = deque([node])
+    explored = set()
+    while frontier:
+        node = frontier.popleft()
+        explored.add(node.state)
+        for child in node.expand(problem):
+            if child.state not in explored and child not in frontier:
+                if problem.goal_test(child.state):
+                    return child
+                frontier.append(child)
+    return None
+
+
+def best_first_graph_search(problem, f):
+    """Search the nodes with the lowest f scores first.
+    You specify the function f(node) that you want to minimize; for example,
+    if f is a heuristic estimate to the goal, then we have greedy best
+    first search; if f is node.depth then we have breadth-first search.
+    There is a subtlety: the line "f = memoize(f, 'f')" means that the f
+    values will be cached on the nodes as they are computed. So after doing
+    a best first search you can examine the f values of the path returned."""
+    f = memoize(f, 'f')
+    node = Node(problem.initial)
+    frontier = PriorityQueue('min', f)
+    frontier.append(node)
+    explored = set()
+    while frontier:
+        node = frontier.pop()
+        if problem.goal_test(node.state):
+            return node
+        explored.add(node.state)
+        for child in node.expand(problem):
+            if child.state not in explored and child not in frontier:
+                frontier.append(child)
+            elif child in frontier:
+                if f(child) < frontier[child]:
+                    del frontier[child]
+                    frontier.append(child)
+    return None
+
+
+def uniform_cost_search(problem):
+    """[Figure 3.14]"""
+    return best_first_graph_search(problem, lambda node: node.path_cost)
+
+
+def depth_limited_search(problem, limit=50):
+    """[Figure 3.17]"""
+
+    def recursive_dls(node, problem, limit):
+        if problem.goal_test(node.state):
+            return node
+        elif limit == 0:
+            return 'cutoff'
+        else:
+            cutoff_occurred = False
+            for child in node.expand(problem):
+                result = recursive_dls(child, problem, limit - 1)
+                if result == 'cutoff':
+                    cutoff_occurred = True
+                elif result is not None:
+                    return result
+            return 'cutoff' if cutoff_occurred else None
+
+    # Body of depth_limited_search:
+    return recursive_dls(Node(problem.initial), problem, limit)
+
+
+def iterative_deepening_search(problem):
+    """[Figure 3.18]"""
+    for depth in range(sys.maxsize):
+        result = depth_limited_search(problem, depth)
+        if result != 'cutoff':
+            return result
+
+
+# ______________________________________________________________________________
+# Bidirectional Search
+# Pseudocode from https://webdocs.cs.ualberta.ca/%7Eholte/Publications/MM-AAAI2016.pdf
+
+def bidirectional_search(problem):
+    e = problem.find_min_edge()
+    gF, gB = {problem.initial: 0}, {problem.goal: 0}
+    openF, openB = [problem.initial], [problem.goal]
+    closedF, closedB = [], []
+    U = infinity
+
+    def extend(U, open_dir, open_other, g_dir, g_other, closed_dir):
+        """Extend search in given direction"""
+        n = find_key(C, open_dir, g_dir)
+
+        open_dir.remove(n)
+        closed_dir.append(n)
+
+        for c in problem.actions(n):
+            if c in open_dir or c in closed_dir:
+                if g_dir[c] <= problem.path_cost(g_dir[n], n, None, c):
+                    continue
+
+                open_dir.remove(c)
+
+            g_dir[c] = problem.path_cost(g_dir[n], n, None, c)
+            open_dir.append(c)
+
+            if c in open_other:
+                U = min(U, g_dir[c] + g_other[c])
+
+        return U, open_dir, closed_dir, g_dir
+
+    def find_min(open_dir, g):
+        """Finds minimum priority, g and f values in open_dir"""
+        m, m_f = infinity, infinity
+        for n in open_dir:
+            f = g[n] + problem.h(n)
+            pr = max(f, 2 * g[n])
+            m = min(m, pr)
+            m_f = min(m_f, f)
+
+        return m, m_f, min(g.values())
+
+    def find_key(pr_min, open_dir, g):
+        """Finds key in open_dir with value equal to pr_min
+        and minimum g value."""
+        m = infinity
+        state = -1
+        for n in open_dir:
+            pr = max(g[n] + problem.h(n), 2 * g[n])
+            if pr == pr_min:
+                if g[n] < m:
+                    m = g[n]
+                    state = n
+
+        return state
+
+    while openF and openB:
+        pr_min_f, f_min_f, g_min_f = find_min(openF, gF)
+        pr_min_b, f_min_b, g_min_b = find_min(openB, gB)
+        C = min(pr_min_f, pr_min_b)
+
+        if U <= max(C, f_min_f, f_min_b, g_min_f + g_min_b + e):
+            return U
+
+        if C == pr_min_f:
+            # Extend forward
+            U, openF, closedF, gF = extend(U, openF, openB, gF, gB, closedF)
+        else:
+            # Extend backward
+            U, openB, closedB, gB = extend(U, openB, openF, gB, gF, closedB)
+
+    return infinity
+
+
+# ______________________________________________________________________________
+# Informed (Heuristic) Search
+
+
+greedy_best_first_graph_search = best_first_graph_search
+
+
+# Greedy best-first search is accomplished by specifying f(n) = h(n).
+
+
+def astar_search(problem, h=None):
+    """A* search is best-first graph search with f(n) = g(n)+h(n).
+    You need to specify the h function when you call astar_search, or
+    else in your Problem subclass."""
+    h = memoize(h or problem.h, 'h')
+    return best_first_graph_search(problem, lambda n: n.path_cost + h(n))
+
+# ______________________________________________________________________________
+# A* heuristics
+
+class LogicElevator(Problem):
+    """ The problem of sliding tiles numbered from 1 to 8 on a 3x3 board,
+    where one of the squares is a blank. A state is represented as a tuple of length 9,
+    where element at index i represents the tile number  at index i (0 if it's an empty square) """
+
+    def __init__(self, initial, goal=(21, 22, 23)):
+        """ Define goal state and initialize a problem """
+
+        self.goal = goal
+        Problem.__init__(self, initial, goal)
+
+    def find_blank_square(self, state):
+        """Return the index of the blank square in a given state"""
+
+        return state.index(0)
+
+    def actions(self, state):
+        """ Return the actions that can be executed in the given state.
+        The result would be a list, since there are only four possible actions
+        in any given state of the environment """
+
+        possible_actions = ['Subir elevadores 1 e 2', 'Subir elevadores 1 e 3',
+                            'Subir elevadores 1 e 4', 'Subir elevadores 1 e 5',
+                            'Subir elevadores 2 e 3', 'Subir elevadores 2 e 4',
+                            'Subir elevadores 2 e 5', 'Subir elevadores 3 e 4',
+                            'Subir elevadores 3 e 5', 'Subir elevadores 4 e 5',
+                            'Descer elevadores 1 e 2', 'Descer elevadores 1 e 3',
+                            'Descer elevadores 1 e 4', 'Descer elevadores 1 e 5',
+                            'Descer elevadores 2 e 3', 'Descer elevadores 2 e 4',
+                            'Descer elevadores 2 e 5', 'Descer elevadores 3 e 4',
+                            'Descer elevadores 3 e 5', 'Descer elevadores 4 e 5']
+
+        if state[0] > 42:
+            possible_actions.remove('Subir elevadores 1 e 2')
+            possible_actions.remove('Subir elevadores 1 e 3')
+            possible_actions.remove('Subir elevadores 1 e 4')
+            possible_actions.remove('Subir elevadores 1 e 5')
+        if state[1] > 42:
+            possible_actions.remove('Subir elevadores 2 e 3')
+            possible_actions.remove('Subir elevadores 2 e 4')
+            possible_actions.remove('Subir elevadores 2 e 5')
+        if state[2] > 42:
+            possible_actions.remove('Subir elevadores 3 e 4')
+            possible_actions.remove('Subir elevadores 3 e 5')
+        if state[3] > 42:
+            possible_actions.remove('Subir elevadores 4 e 5')
+        if state[0] < 12:
+            possible_actions.remove('Descer elevadores 1 e 2')
+            possible_actions.remove('Descer elevadores 1 e 3')
+            possible_actions.remove('Descer elevadores 1 e 4')
+            possible_actions.remove('Descer elevadores 1 e 5')
+        if state[1] < 12:
+            possible_actions.remove('Descer elevadores 2 e 3')
+            possible_actions.remove('Descer elevadores 2 e 4')
+            possible_actions.remove('Descer elevadores 2 e 5')
+        if state[2] < 12:
+            possible_actions.remove('Descer elevadores 3 e 4')
+            possible_actions.remove('Descer elevadores 3 e 5')
+        if state[3] < 12:
+            possible_actions.remove('Descer elevadores 4 e 5')
+
+        return possible_actions
+
+    def result(self, state, action):
+        """ Given state and action, return a new state that is the result of the action.
+        Action is assumed to be a valid action in the state """
+
+        # blank is the index of the blank square
+        new_state = list(state)
+        do_action = action.split()
+
+        if do_action[0] == 'Subir':
+            new_state[int(do_action[2])-1] += 8
+            new_state[int(do_action[4])-1] += 8
+        elif do_action[0 == 'Descer']:
+            new_state[int(do_action[2])-1] -= 13
+            new_state[int(do_action[4])-1] -= 13
+
+        return tuple(new_state)
+
+    def goal_test(self, state):
+        """ Given a state, return True if state is a goal state or False, otherwise """
+        return all(floor in self.goal for floor in state)
+
+    def h(self, node):
+        """ Return the heuristic value for a given state. Default heuristic function used is
+        h(n) = number of misplaced tiles """
+        return 0
+        #return sum(s != g for (s, g) in zip(node.state, self.goal))
+
+
+# ______________________________________________________________________________
+iterative_deepening_search(LogicElevator((20, 20, 24, 22, 21)))
